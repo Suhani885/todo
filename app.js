48aec747d5320a1b52a7f516b1e1b6db463223e5
Suhani <suhaniss885@gmail.com>
2024-09-16 10:04:10 +0530

    second commit

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ app.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider,
         .state('register', {
             url: '/register',
             templateUrl: 'register.html',
-            controller: 'RegisterController',
+            controller: 'RegController',
             controllerAs: 'regCtrl'
         })
         .state('todo', {
@@ -24,28 +24,142 @@ app.config(['$urlRouterProvider', '$stateProvider', function($urlRouterProvider,
         });
 }]);
 
-// app.controller('LoginController', function() {
-//     this.login = function() {
-//         console.log('Login attempted');
+app.controller('RegController', ['$http', '$state', function ($http, $state) {
+    var regCtrl = this;
+    regCtrl.register = function() {
+        console.log(regCtrl.fname, regCtrl.lname, regCtrl.username, regCtrl.email, regCtrl.pass1, regCtrl.pass2);
+        if (regCtrl.pass1 !== regCtrl.pass2) {
+            regCtrl.errorMessage = "Passwords do not match";
+            return;
+        }
+        var req = {
+            method: 'POST',
+            url: 'http://10.21.97.17:8000/todo/register/',
+            headers: {
+                'Content-Type': "application/json"
+            },
+            data: {
+                "Fname": regCtrl.fname,
+                "Lname": regCtrl.lname,
+                "Uname": regCtrl.username,
+                "Email": regCtrl.email,
+                "Passwd1": regCtrl.pass1,
+                "Passwd2": regCtrl.pass2
+            }
+        };
+        $http(req).then(function(response) {
+            console.log(response);
+            regCtrl.successMessage = "Registration successful!";
+            $state.go('login');
+        }, function(error) {
+            regCtrl.errorMessage = "Registration failed. Please try again.";
+            console.log("error", error);
+        });
+    };
+}]);
 
+app.controller('LoginController', ['$http','$state', function ($http,$state) {
+    var loginCtrl = this;
+    loginCtrl.login = function() {
+        console.log(loginCtrl.username, loginCtrl.password);
+        if (loginCtrl.username && loginCtrl.password) {
+            var req = {
+                method: 'POST',
+                url: 'http://10.21.97.17:8000/todo/login/',
+                headers: {
+                    'Content-Type': "application/json"
+                },
+                data: {
+                    "Uname": loginCtrl.username,
+                    "Passwd": loginCtrl.password
+                }
+            };
+            $http(req).then(function(response) {
+                console.log(response.data);
+                loginCtrl.successMessage = "Login successful!";
+                sessionStorage.setItem('username', response.data.username);
+                $state.go('todo');
+            }, function(error) {
+                loginCtrl.errorMessage = "Error";
+                console.log("error", error);
+            });
+        } else {
+            loginCtrl.errorMessage = "Enter username and password";
+        }
+    };
+}]);
+
+app.controller('TodoController', ['$http', '$state', function($http, $state) {
+    this.logout = function() {
+            var req = {
+                method: 'POST',
+                url: 'http://10.21.97.17:8000/todo/todo/',
+                headers: {
+                    'Content-Type': "application/json"
+                },
+                data: {
+                    "Todo": "logout",
+                    "Status":"logout"
+                }
+            };
+            $http(req).then(function(response) {
+                console.log(response);
+                sessionStorage.removeItem('username');
+                $state.go('login');
+            }, function(error) {
+                console.log("error", error);
+            });
+    };
+}]);
+
+// app.controller('TodoController', ['$http', '$state', function($http, $state) {
+//     var todoCtrl = this;
+//     todoCtrl.todos = [];
+//     todoCtrl.newTodo = '';
+//     todoCtrl.username = sessionStorage.getItem('username') || 'User';
+
+//     todoCtrl.fetchTodos = function() {
+//         $http.get('http://10.21.97.17:8000/todo/todo/')
+//             .then(function(response) {
+//                 todoCtrl.todos = response.data;
+//             }, function(error) {
+//                 console.log("Error fetching todos:", error);
+//             });
 //     };
-// });
+//     todoCtrl.fetchTodos();
 
-// app.controller('RegisterController', function() {
-//     this.register = function() {
-//         console.log('Registration attempted');
-        
-//     };
-// });
-
-// app.controller('TodoController', function() {
-//     this.todos = [];
-//     this.newTodo = '';
-
-//     this.addTodo = function() {
-//         if (this.newTodo) {
-//             this.todos.push({text: this.newTodo, done: false});
-//             this.newTodo = '';
+//     todoCtrl.addTodo = function() {
+//         if (todoCtrl.newTodo) {
+//             $http.post('http://10.21.97.17:8000/todo/todo/', { text: todoCtrl.newTodo })
+//                 .then(function(response) {
+//                     todoCtrl.todos.push(response.data);
+//                     todoCtrl.newTodo = '';
+//                 }, function(error) {
+//                     console.log("Error adding todo:", error);
+//                 });
 //         }
 //     };
-// });
+
+    // todoCtrl.updateTodo = function(index) {
+    //     var updatedText = prompt("Update todo:", todoCtrl.todos[index].text);
+    //     if (updatedText !== null) {
+    //         $http.put('http://10.21.97.17:8000/todo/todo/' + todoCtrl.todos[index].id, { text: updatedText })
+    //             .then(function(response) {
+    //                 todoCtrl.todos[index].text = updatedText;
+    //             }, function(error) {
+    //                 console.log("Error updating todo:", error);
+    //             });
+    //     }
+    // };
+
+    // todoCtrl.deleteTodo = function(index) {
+    //     if (confirm("Are you sure you want to delete this todo?")) {
+    //         $http.delete('http://10.21.97.17:8000/todo/todo/' + todoCtrl.todos[index].id)
+    //             .then(function(response) {
+    //                 todoCtrl.todos.splice(index, 1);
+    //             }, function(error) {
+    //                 console.log("Error deleting todo:", error);
+    //             });
+    //     }
+    // };
+// }]);
